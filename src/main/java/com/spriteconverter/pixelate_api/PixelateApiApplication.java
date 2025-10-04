9cfc4c7eea0f70c82f630c0eb17045ada3c8584b
gabs <gabriel.alfonso2001@gmail.com>
2025-10-04 13:09:49 +0200

    trout commit

---
+++ src/main/java/com/spriteconverter/pixelate_api/PixelateApiApplication.java
@@ -0,0 +1,13 @@
+package com.spriteconverter.pixelate_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelateApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spriteconverter/pixelate_api/PixelateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spriteconverter.pixelate_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
